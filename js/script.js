37657fe542fea0fc3fc2b850ee213d795ec3fbde
v_code <12273445-v_code@users.noreply.gitlab.com>
2023-07-29 18:55:48 +0300

    create note

--- js/script.js
+++ js/script.js
@@ -40,13 +40,12 @@ document.addEventListener("DOMContentLoaded", function() {
     ];
 
     const tableBody = document.getElementById("table-body");
+    const addNoteModal = new bootstrap.Modal(document.getElementById("addNoteModal"));
 
     function createTableRow(data) {
         const row = document.createElement("tr");
-
         for (const key in data) {
             const cell = document.createElement("td");
-
             if (key === "icons") {
                 data[key].forEach(iconClass => {
                 const icon = document.createElement("i");
@@ -59,12 +58,74 @@ document.addEventListener("DOMContentLoaded", function() {
 
             row.appendChild(cell);
         }
-
         return row;
     }
 
-    rowData.forEach(data => {
-        const row = createTableRow(data);
-        tableBody.appendChild(row);
+    function updateTable() {
+        tableBody.innerHTML = "";
+        rowData.forEach(data => {
+          const row = createTableRow(data);
+          tableBody.appendChild(row);
+        });
+    }
+
+    const createNoteButton = document.getElementById("create-note-btn");
+    createNoteButton.addEventListener("click", function() {
+        document.getElementById("noteName").value = "";
+        document.getElementById("noteContent").value = "";
+
+        const noteCategorySelect = document.getElementById("noteCategory");
+        noteCategorySelect.value = "Task";
+
+        const addNoteModal = new bootstrap.Modal(document.getElementById("addNoteModal"));
+        addNoteModal.show();
     });
+
+    const saveNoteButton = document.getElementById("saveNoteBtn");
+    saveNoteButton.addEventListener("click", function() {
+        const name = document.getElementById("noteName").value;
+        const content = document.getElementById("noteContent").value;
+        const category = document.getElementById("noteCategory").value;
+
+        const currentDate = new Date();
+        const createdOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+        const formattedCreated = currentDate.toLocaleDateString('en-US', createdOptions);
+
+        const datesRegex = /\b\d{1,2}\/\d{1,2}\/\d{4}\b/g;
+        const datesArray = content.match(datesRegex);
+
+        const formattedDates = datesArray ? datesArray.map(dateStr => {
+            const dateParts = dateStr.split('/');
+            return `${dateParts[0]}/${dateParts[1]}/${dateParts[2]}`;
+          }) : [];
+
+        const newNote = {
+        name: name,
+        created: formattedCreated,
+        category: category,
+        content: content,
+        dates: formattedDates.join(', '),
+        icons: [
+            "bi bi-pencil-square",
+            "bi bi-archive-fill",
+            "bi bi-trash-fill"
+        ]
+        };
+        rowData.push(newNote);
+        updateTable();
+
+        addNoteModal.hide();
+    });
+
+    const cancelNoteButton = document.getElementById("cancelNoteBtn");
+    cancelNoteButton.addEventListener("click", function() {
+        addNoteModal.hide();
+    });
+
+    const closeModalButton = document.getElementById("closeModalButton");
+    closeModalButton.addEventListener('click', function() {
+        addNoteModal.hide();
+    })
+
+    updateTable();
 });
